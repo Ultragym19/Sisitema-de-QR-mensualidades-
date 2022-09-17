c32238e5c1cff956efd3d7890f7773dc880b852a
Ultragym19 <113781938+Ultragym19@users.noreply.github.com>
2022-09-17 09:17:23 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AquiVaTuURL/exec";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbz7PmCz37nTy6xNavhEV8cgy8ZhnsaZnQ7zIv9TucxbvubOfmsb5SkU17moZ0Z9FWJw/exec";
